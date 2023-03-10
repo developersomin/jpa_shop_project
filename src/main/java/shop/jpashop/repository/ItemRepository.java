1139a312dce72e838e1be0d54716546867d47874
ahngiwon00 <angiwon12345@nate.com>
2023-03-11 01:17:25 +0900

    상품 관리

--- src/main/java/shop/jpashop/repository/ItemRepository.java
+++ src/main/java/shop/jpashop/repository/ItemRepository.java
@@ -1,7 +1,8 @@
 package shop.jpashop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import shop.jpashop.entity.Item;
 
-public interface ItemRepository extends JpaRepository<Item,Long>,ItemRepositoryCustom {
+public interface ItemRepository extends JpaRepository<Item,Long>, QuerydslPredicateExecutor<Item>,ItemRepositoryCustom {
 }
--- src/main/java/shop/jpashop/repository/ItemRepositoryCustomImpl.java
+++ src/main/java/shop/jpashop/repository/ItemRepositoryCustomImpl.java
@@ -5,6 +5,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.util.StringUtils;
 import shop.jpashop.dto.ItemSearchDto;
 import shop.jpashop.entity.Item;
 import shop.jpashop.entity.ItemSellStatus;
@@ -26,11 +27,11 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
         return searchSellStatus == null ? null : QItem.item.itemSellStatus.eq(searchSellStatus);
     }
 
-    private BooleanExpression createDateAfter(String searchDateType){
+    private BooleanExpression regDtsAfter(String searchDateType){
 
         LocalDateTime dateTime = LocalDateTime.now();
 
-        switch (searchDateType) {
+        /*switch (searchDateType) {
             case "1d":
                 dateTime = dateTime.minusDays(1);
                 break;
@@ -45,28 +46,50 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
                 break;
             default:
                 return null;
+        }*/
+
+        if (searchDateType == "1d") {
+            dateTime = dateTime.minusDays(1);
+        } else if (searchDateType =="1w") {
+            dateTime = dateTime.minusWeeks(1);
+        }
+        else if (searchDateType =="1m") {
+            dateTime = dateTime.minusMonths(1);
+        } else if (searchDateType == "6m") {
+            dateTime = dateTime.minusMonths(6);
+        } else {
+            return null;
         }
         return QItem.item.createdDate.after(dateTime);
     }
 
     private BooleanExpression searchByLike(String searchBy, String searchQuery) {
-        switch (searchBy) {
+
+        /*switch (searchBy) {
             case "itemName":
                 return QItem.item.itemName.like("%" + searchQuery + "%");
             case "createBy":
                 return QItem.item.createBy.like("%" + searchQuery + "%");
             default:
                 return null;
+        }*/
+
+        if (searchBy == "itemName") {
+            return QItem.item.itemName.like("%" + searchQuery + "%");
+        } else if (searchBy == "createBy") {
+            return QItem.item.createBy.like("%" + searchQuery + "%");
+        } else {
+            return null;
         }
     }
 
     @Override
     public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
         List<Item> content = queryFactory
-                .select(QItem.item)
+                .selectFrom(QItem.item)
                 .where(searchSellStatusEq(itemSearchDto.getSearchSellStatus()),
-                        createDateAfter(itemSearchDto.getSearchDateType()),
-                        searchByLike(itemSearchDto.getSearchBy(), itemSearchDto.getSearchQuery()))
+                        searchByLike(itemSearchDto.getSearchBy(), itemSearchDto.getSearchQuery()),
+                        regDtsAfter(itemSearchDto.getSearchDateType()))
                 .orderBy(QItem.item.id.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
